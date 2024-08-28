887bc5fc24ff8407ac23218dc10eb03ebb5e6b5f
hwa.j <hwajin0720@naver.com>
2024-08-29 00:20:04 +0900

    세션으로 로그인

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -11,18 +11,34 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 
+import jakarta.servlet.http.HttpSession;
+
 @RestController
 public class UserApiController {
 
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
-		System.out.println("UserApiController : save 함수 호출됨.");
+		System.out.println("UserApiController : save 호출됨.");
 		
 		user.setRole(RoleType.USER);
 		int result = userService.save(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(),result);
+	}
+	
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user){
+		System.out.println("UserApiController : login 호출됨.");
+		User principal = userService.login(user); //principal(접근주체)
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal); //세션 생성 
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 	}
 }
