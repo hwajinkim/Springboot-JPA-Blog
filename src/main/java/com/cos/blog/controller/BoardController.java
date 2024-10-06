f6b8f1d9600ab512609fc10df26062e7e2974c9a
hwa.j <hwajin0720@naver.com>
2024-10-06 17:41:45 +0900

    블로그 글수정

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -32,6 +32,12 @@ public class BoardController {
 		return "board/detail";
 	}
 	
+	@GetMapping("/board/{id}/updateForm")
+	public String updateForm(@PathVariable int id, Model model) {
+		model.addAttribute("board", boardService.view(id));
+		return "board/updateForm";
+	}
+	
 	// USER 권한 필요
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
