caa2b0949bdaf49a8dfde04099d226d0fae638be
hwa.j <hwajin0720@naver.com>
2024-10-06 15:08:46 +0900

    블로그 글목록

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,16 +1,24 @@
 package com.cos.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
+
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
 
 	@GetMapping({"","/"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("boards", boardService.list());
 		return "index";
 	}
 	
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.cos.blog.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -30,5 +32,9 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+	
+	public List<Board> list(){
+		return boardRepository.findAll();
 	}
 }
